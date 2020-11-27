df29b56339f73d0808ebb3bb8d7503795002ac6f
timeshell <huxiaoyanghfut@gmail.com>
2020-11-27 13:58:35 +0800

    完善画图部分

--- chartGanttCreator.py
+++ chartGanttCreator.py
@@ -15,15 +15,16 @@ def createGanttChart(aJobsList, machinesList, itinerariesList, time=0):
     ax.set_xlabel('Time', fontsize=12)  # description of axis and chart title
     ax.set_ylabel('Machines', fontsize=12)
     chartTitle = "Gantt chart  Cmax=" + str(cMax) +"\n"
+    plt.title(chartTitle, fontsize=12)
 
+
+    chartDetails = ""
     for i, itinerary in enumerate(itinerariesList):
         arr = [job for job in aJobsList if job.itinerary == itinerary.name]
         arr.sort(key=lambda x: x.endTime)
-        chartTitle = chartTitle + " C" + str(arr[-1].itinerary[-1]) + "=" + str(arr[-1].endTime) + ","
-        if not fmod(i, 10) and i != 0:
-            chartTitle = chartTitle + "\n"
+        chartDetails = chartDetails + " C" + str(arr[-1].itinerary[-1]) + "=" + str(arr[-1].endTime) + ","
 
-    plt.title(chartTitle, fontsize=10)
+    plt.suptitle(chartDetails, fontsize=10)
 
 
     # values for machines y axis
@@ -45,6 +46,9 @@ def createGanttChart(aJobsList, machinesList, itinerariesList, time=0):
     for job in uniqueItinerariesInJobList:
         legendsColors.append(mpatches.Patch(color=job.colorOfItinerary, label=job.itinerary))  # legend color and name
     plt.legend(handles=legendsColors, fontsize=8)
+
+
+
     tuplesForMachineAxis = []
     colorsForMachineAxis = []
     for index, machLabel in enumerate(machinesNamesRev):
@@ -55,6 +59,7 @@ def createGanttChart(aJobsList, machinesList, itinerariesList, time=0):
         ax.broken_barh(tuplesForMachineAxis, ((index + 1) * 10, 9), facecolors=colorsForMachineAxis)
         tuplesForMachineAxis.clear()
         colorsForMachineAxis.clear()
+
     ax.xaxis.set_major_locator(ticker.MultipleLocator(10))  # this is cosmetics to set unit to 10
     for label in ax.get_xticklabels()[::2]:  # and show only every two ticks
         label.set_visible(False)
